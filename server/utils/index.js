9ffd91618eeb0a215c2045077e57ee255cb6ba29
nypinstripes <nypinstripes@gmail.com>
2019-02-07 02:14:23 -0500

    Re-insert uniqKey helper.

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,5 +1,9 @@
 const agents = require('../../shared/data/browsers');
 
+exports.getItemArrayIndex = params => {
+  return params.items.findIndex(item => item.id === params.id);
+};
+
 exports.isBrowserSupported = parser => {
   const { browser: { name }, ua } = parser;
   let browsers = agents.browsers.map(browser => browser.agents.join(' '));
@@ -14,4 +18,11 @@ exports.isBrowserSupported = parser => {
   }
 
   return support;
+};
+
+exports.uniqKey = () => {
+  let firstPart = (new Date()).getTime().toString(36);
+  let lastPart = Math.random().toString(36).substring(2);
+
+  return `${firstPart}${lastPart}`;
 };
